7864e05a25a2c32dcd53a0ead63a6f54c208a193
George Burduli <burduli94@gmail.com>
2022-09-18 19:18:06 +0400

    added styling options in widget settings

--- includes/frt-weather-widget.php
+++ includes/frt-weather-widget.php
@@ -33,9 +33,19 @@ class Widget extends \WP_Widget
         } else {
             return;
         }
+
+        $backgroundColor = apply_filters('background_color', $instance['backgroundColor']);
+        $textColor = apply_filters('text_color', $instance['textColor']);
+        $border = apply_filters('border', $instance['border']);
+        $borderRadius = apply_filters('border_radius', $instance['borderRadius']);
         ?>
 
-        <div>
+        <div
+            style="padding: 10px;
+                    background-color: <?php echo $backgroundColor; ?>;
+                    color: <?php echo $textColor; ?>;
+                    border: <?php echo $border; ?>;
+                    border-radius: <?php echo $borderRadius; ?>">
             <?php if (isset($options['frtwp_weather_condition_field'])) : ?>
             <div>
                 <img
@@ -78,6 +88,68 @@ class Widget extends \WP_Widget
                 <p>Sunset: <?php echo gmdate("Y-m-d\TH:i:s\Z", $res->sys->sunset); ?></p>
             <?php endif; ?>
         </div>
+        <?php
+    }
+
+    public function form($instance)
+    {
+        $backgroundColor = isset($instance['backgroundColor'])
+        ? $instance['backgroundColor']
+        : __('Background color', 'frt-weather-plugin');
+        
+        $textColor = isset($instance['textColor'])
+        ? $instance['textColor']
+        : __('Text color', 'frt-weather-plugin');
+
+        $border = isset($instance['border'])
+        ? $instance['border']
+        : __('Widget border', 'frt-weather-plugin');
+
+        $borderRadius = isset($instance['borderRadius'])
+        ? $instance['borderRadius']
+        : __('Widget border radius', 'frt-weather-plugin');
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_name('backgroundColor'); ?>">
+                <?php _e('Background color', 'frt-weather-plugin'); ?>
+            </label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('backgroundColor'); ?>"
+                name="<?php echo $this->get_field_name('backgroundColor'); ?>"
+                value="<?php echo esc_attr($backgroundColor); ?>" />
+
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_name('textColor'); ?>">
+                <?php _e('Text color', 'frt-weather-plugin'); ?>
+            </label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('textColor'); ?>"
+                name="<?php echo $this->get_field_name('textColor'); ?>"
+                value="<?php echo esc_attr($textColor); ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_name('border'); ?>">
+                <?php _e('Border', 'frt-weather-plugin'); ?>
+            </label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('border'); ?>"
+                name="<?php echo $this->get_field_name('border'); ?>"
+                value="<?php echo esc_attr($border); ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_name('borderRadius'); ?>">
+                <?php _e('Border radius', 'frt-weather-plugin'); ?>
+            </label>
+            <input
+                type="text"
+                id="<?php echo $this->get_field_id('borderRadius'); ?>"
+                name="<?php echo $this->get_field_name('borderRadius'); ?>"
+                value="<?php echo esc_attr($borderRadius); ?>" />
+        </p>
         <?php
     }
 }
