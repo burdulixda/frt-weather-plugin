f9498973dfa75c650195e90655e92effbf3a840f
George Burduli <burduli94@gmail.com>
2022-09-18 04:29:44 +0400

    added widget settings

--- includes/frt-weather-settings.php
+++ includes/frt-weather-settings.php
@@ -20,6 +20,13 @@ class Settings
             array($this, 'FRTWPSectionGeneralCallback'),
             'frtwp'
         );
+
+        add_settings_section(
+            'frtwp_section_weather',
+            __('Weather', 'frt-weather-plugin'),
+            array($this, 'FRTWPSectionWeatherCallback'),
+            'frtwp'
+        );
         
         add_settings_field(
             'frtwp_api_key_field',
@@ -32,11 +39,60 @@ class Settings
                 'class' => 'frtwp_row',
             )
         );
+
+        add_settings_field(
+            'frtwp_temperature_unit_field',
+            __('Temperature unit', 'frt-weather-plugin'),
+            array($this, 'FRTWPTemperatureUnitFieldCallback'),
+            'frtwp',
+            'frtwp_section_weather',
+            array(
+                'label_for' => 'frtwp_temperature_unit_field',
+                'class' => 'frtwp_row'
+            )
+        );
+
+        $this->FRTWPSettingsFields();
+    }
+
+    public function FRTWPSettingsFields()
+    {
+        $fields = [
+            'temperature' => __('Temperature', 'frt-weather-plugin'),
+            'temperature_scale' => __('Temperature scale', 'frt-weather-plugin'),
+            'pressure' => __('Pressure', 'frt-weather-plugin'),
+            'wind' => __('Wind', 'frt-weather-plugin'),
+            'visibility' => __('Visibility', 'frt-weather-plugin'),
+            'sunrise' => __('Sunrise', 'frt-weather-plugin'),
+            'sunset' => __('Sunset', 'frt-weather-plugin')
+        ];
+
+        foreach ($fields as $field => $label) {
+            add_settings_field(
+                'frtwp_' . $field . '_field',
+                $label,
+                array($this, 'FRTWPCheckboxFieldsCallback'),
+                'frtwp',
+                'frtwp_section_weather',
+                array(
+                    'label_for' => 'frtwp_' . $field . '_field',
+                    'class' => 'frtwp_row',
+                )
+            );
+        }
     }
     
     public function FRTWPSectionGeneralCallback()
     {
-        esc_html_e("whatever");
+        esc_html_e("Configure plugin settings.", "frt-weather-plugin");
+    }
+
+    public function FRTWPSectionWeatherCallback()
+    {
+        esc_html_e(
+            "Configure weather widget settings. Tick the options you wish to show in the weather widget.",
+            "frt-weather-plugin"
+        );
     }
 
     public function FRTWPApiKeyFieldCallback($args)
@@ -48,6 +104,31 @@ class Settings
             id="<?php echo esc_attr($args['label_for']); ?>"
             name="frtwp_options[<?php echo esc_attr($args['label_for']); ?>]"
             value="<?php echo isset($options[$args['label_for']]) ? esc_attr($options[$args['label_for']]) : ''; ?>" />
+        <?php
+    }
+
+    public function FRTWPTemperatureUnitFieldCallback($args) {
+        $options = get_option('frtwp_options');
+        ?>
+        <select
+            name="frtwp_options[<?php echo esc_attr($args['label_for']); ?>]"
+            id="<?php echo esc_attr($args['label_for']); ?>" >
+            <option value="C" <?php selected("C", $options[$args['label_for']]); ?>>Celsius</option>
+            <option value="F" <?php selected("F", $options[$args['label_for']]); ?>>Fahrenheit</option>
+        </select>
+        <?php
+    }
+
+    public function FRTWPCheckboxFieldsCallback($args)
+    {
+        $options = get_option('frtwp_options');
+        ?>
+        <input
+            type="checkbox"
+            id="<?php echo esc_attr($args['label_for']); ?>"
+            name="frtwp_options[<?php echo esc_attr($args['label_for']); ?>]"
+            value="1"
+            <?php isset($options[$args['label_for']]) ? checked(1, $options[$args['label_for']]) : ''; ?> />
         <?php
     }
 
@@ -69,7 +150,12 @@ class Settings
         }
     
         if (isset($_GET['settings-updated'])) {
-            add_settings_error('frtwp_messages', 'frtwp_success', __('Settings Saved', 'frt-weather-plugin'), 'success');
+            add_settings_error(
+                'frtwp_messages',
+                'frtwp_success',
+                __('Settings Saved', 'frt-weather-plugin'),
+                'success'
+            );
         }
     
         settings_errors('frtwp_messages'); ?>
